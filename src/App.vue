a6f2b6ee99c4ed12b223d1545cee92edc6d262c4
Julia <stueme@web.de>
2025-06-09 22:42:38 +0200

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+
+<template>
+  <div class="container">
+    <TermineAnzeige />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TermineAnzeige from './components/TermineAnzeige.vue'
+</script>
