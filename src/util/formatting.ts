99765fa90138b66893e6b116d7d4689c26587d03
Julia <stueme@web.de>
2025-08-10 18:38:15 +0200

    Wochentag in die Übersicht

--- src/util/formatting.ts
+++ src/util/formatting.ts
@@ -1,16 +1,19 @@
 import { Termin } from '../types/termin';
 
 export function formatDatum(termin: Termin): string {
- 
-    const date = new Date(termin.datum);
-    const time = termin.uhrzeit ? termin.uhrzeit : date.toLocaleTimeString('de-DE', {
-      hour: '2-digit',
-      minute: '2-digit',
-    });
 
-    return date.toLocaleDateString('de-DE', {
-      year: 'numeric',
-      month: 'long',
-      day: 'numeric',
-    }) + ' ' + time + ' Uhr';
-  }
+  const date = new Date(termin.datum);
+  const time = termin.uhrzeit ? termin.uhrzeit : date.toLocaleTimeString('de-DE', {
+    hour: '2-digit',
+    minute: '2-digit',
+  });
+
+  const wochentage = ['So', 'Mo', 'Di', 'Mi', 'Do', 'Fr', 'Sa'];
+  const tag = wochentage[date.getDay()];
+
+  return tag + ', ' + date.toLocaleDateString('de-DE', {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+  }) + ' ' + time + ' Uhr';
+}
